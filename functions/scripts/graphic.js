46cf80c06311112ac167f9e775dec813d9f77fcb
JavierMorales0 <javiermoralesmelara@gmail.com>
2022-01-25 12:29:50 -0600

    Fix bugs

--- functions/scripts/graphic.js
+++ functions/scripts/graphic.js
@@ -2,7 +2,14 @@ function graphic(data) {
   let graphic = {};
   data.forEach((element) => {
     // Primero se recorta la fecha
-    element._id = element._id.toISOString().substring(0, 10);
+    const temporalDate = element._id
+      .toISOString()
+      .substring(0, 10)
+      .replace(/-/g, "/")
+      .split("/")
+      .reverse();
+    const date = temporalDate[0] + "/" + temporalDate[1];
+    element._id = date;
     // Se asocia con un objeto de tipo clave - valor
     graphic[element._id] = element.total;
   });
