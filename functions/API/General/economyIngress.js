e27dce491c95a6a12d11b2c3501619d5eda08b3e
JavierMorales0 <javiermoralesmelara@gmail.com>
2022-01-27 17:12:54 -0600

    Agregado el endpoint de extra moves y su aplicacion en el apartado de ingresos

--- functions/API/General/economyIngress.js
+++ functions/API/General/economyIngress.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const route = express.Router();
 const Sales = require("../../db/Models/Inventory/Sale");
 const { graphic } = require("../../scripts/graphic");
@@ -8,10 +9,13 @@ const {
   getDataLastThreeMonths,
   verifyDataForPercentage,
 } = require("../../scripts/statistics");
-
+const ExtraMove = require("../../db/Models/General/ExtraMove");
+// Se declara el valor del tipo de movimiento para ingresos
+var val = mongoose.Types.ObjectId("61dc6d250dea196d5fdf0bf7");
 route.get("/", async (req, res) => {
   try {
     let sales;
+    let extraMoves;
     // Verificando si consulta rangos de fechas
     if (req.query.startDate && req.query.endDate) {
       // Obtener las ventas y formatearlo con el rango de fecha dado
@@ -22,6 +26,26 @@ route.get("/", async (req, res) => {
               $gte: new Date(req.query.startDate),
               $lte: new Date(req.query.endDate),
             },
+          },
+        },
+        {
+          $group: {
+            _id: "$date",
+            total: { $sum: "$total" },
+          },
+        },
+        {
+          $sort: { _id: 1 },
+        },
+      ]);
+      extraMoves = await ExtraMove.aggregate([
+        {
+          $match: {
+            date: {
+              $gte: new Date(req.query.startDate),
+              $lte: new Date(req.query.endDate),
+            },
+            type_move: val,
           },
         },
         {
@@ -47,23 +71,58 @@ route.get("/", async (req, res) => {
           $sort: { _id: 1 },
         },
       ]);
+      // Obtener extraMoves donde la fecha sea igual a la fecha de venta y sea de tipo ingreso
+      extraMoves = await ExtraMove.aggregate([
+        {
+          $match: {
+            type_move: val,
+          },
+        },
+        {
+          $group: {
+            _id: "$date",
+            total: { $sum: "$total" },
+          },
+        },
+        {
+          $sort: { _id: 1 },
+        },
+      ]);
     }
     // Graficar los datos
     const salesGraphic = graphic(sales);
+    const extraMovesGraphic = graphic(extraMoves);
 
-    // Obtener los movimientos extras y formatearlo
+    // Verificar que son arrays
+    if (!Array.isArray(sales)) {
+      const pivot = sales;
+      sales = [];
+      sales.push(pivot);
+    }
+    if (!Array.isArray(extraMoves)) {
+      const pivot = extraMoves;
+      extraMoves = [];
+      extraMoves.push(pivot);
+    }
 
     // Obtener totales
-    const totalSales = total(sales);
+    const totalSales = parseFloat(total(sales));
+    const totalExtraMoves = parseFloat(total(extraMoves));
 
-    // Redondear a dos decimales
-    const totalGeneral = Math.round(totalSales * 100) / 100;
+    // Obtener total general
+    let totalGeneral = totalSales + totalExtraMoves;
 
     // Asignar fecha de inicio y fin
     const salesDates = checkDates(
       req.query.startDate,
       req.query.endDate,
       sales
+    );
+
+    const extraMovesDates = checkDates(
+      req.query.startDate,
+      req.query.endDate,
+      extraMoves
     );
 
     // Obtener datos estadísticos
@@ -84,7 +143,13 @@ route.get("/", async (req, res) => {
         endDate: salesDates.endDate,
         filtered: salesDates.filtered,
       },
-      extraMoves: {},
+      extraMoves: {
+        graphic: extraMovesGraphic,
+        total: totalExtraMoves,
+        startDate: extraMovesDates.startDate,
+        endDate: extraMovesDates.endDate,
+        filtered: extraMovesDates.filtered,
+      },
     };
     return res.status(200).json(response);
   } catch (error) {
--- functions/API/General/extraMoves.js
+++ functions/API/General/extraMoves.js
@@ -3,41 +3,118 @@ const route = express.Router();
 const ExtraMove = require("../../db/Models/General/ExtraMove");
 const { errors } = require("../../middleware/errors");
 let { authenticateToken } = require("../../middleware/auth");
-const { body } = require("express-validator");
+const { body, param, validationResult } = require("express-validator");
 
 route.get("/", async (req, res) => {
   try {
     let extraMoves = await ExtraMove.find()
-      //.populate("Production")
-      .populate("TypeMove");
-    res.status(200).json(extraMoves);
+      // Popular con typeMove
+      .populate("type_move", { title: 1, _id: 1 });
+    return res.status(200).json(extraMoves);
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
       name: error.name,
       message: error.message,
     });
   }
 });
 
-route.post("/", async (req, res) => {
-  errors.validationErrorResponse(req, res);
-  try {
-    const { description, date, type_move, production, total } = req.body;
-    const extraMove = new ExtraMove({
-      description,
-      date,
-      type_move,
-      total,
-    });
-    await extraMove.save();
-    res.status(200).json(extraMove);
-  } catch (err) {
-    res.status(500).json({
-      name: err.name,
-      message: err.message,
-    });
+route.post(
+  "/",
+  body("description").notEmpty().withMessage("Descripción es requerida"),
+  body("type_move").notEmpty().withMessage("Tipo de movimiento es requerido"),
+  body("type_move").isMongoId().withMessage("Tipo de movimiento no válido"),
+  body("date").isDate().withMessage("Fecha de movimiento es requerida"),
+  body("total").notEmpty().withMessage("Total es requerido"),
+  body("total").isNumeric().withMessage("Total debe ser un número"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    try {
+      const { description, date, type_move, total } = req.body;
+      const extraMove = new ExtraMove({
+        description,
+        date,
+        type_move,
+        total,
+      });
+      await extraMove.save();
+      return res.status(200).json(extraMove);
+    } catch (err) {
+      return res.status(500).json({
+        name: err.name,
+        message: err.message,
+      });
+    }
   }
-});
+);
+
+// Cambiar el tipo de movimiento
+route.put(
+  "/:id/:type",
+  // Validar el parametro id
+  param("id").isMongoId().withMessage("Id debe ser un id valido"),
+  // Validar el parametro type
+  param("type").isMongoId().withMessage("Tipo debe ser un id valido"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    try {
+      const { id, type } = req.params;
+      const extraMove = await ExtraMove.findById(id);
+      if (!extraMove) {
+        return res.status(404).json({
+          name: "Movimiento extra",
+          message: "No se encontró el movimiento extra",
+        });
+      }
+      extraMove.type_move = type;
+      await extraMove.save();
+      return res.status(200).json(extraMove);
+    } catch (error) {
+      return res.status(500).json({
+        name: error.name,
+        message: error.message,
+      });
+    }
+  }
+);
+
+// Cambiar el total
+route.put(
+  "/:id",
+  body("total").notEmpty().withMessage("Total es requerido"),
+  body("total").isDecimal().withMessage("Total debe ser un número"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    try {
+      const { id } = req.params;
+      const { total } = req.body;
+      const extraMove = await ExtraMove.findById(id);
+      if (!extraMove) {
+        return res.status(404).json({
+          name: "Movimiento extra",
+          message: "No se encontró el movimiento extra",
+        });
+      }
+      extraMove.total = total;
+      await extraMove.save();
+      return res.status(200).json(extraMove);
+    } catch (error) {
+      return res.status(500).json({
+        name: error.name,
+        message: error.message,
+      });
+    }
+  }
+);
 
 route.delete("/:id", async (req, res) => {
   try {
