2fb0cd9053fb8634298370d910caca250dfabf1e
JavierMorales0 <javiermoralesmelara@gmail.com>
2022-02-01 11:34:12 -0600

    Arreglado un bug

--- functions/db/Models/Inventory/Sale.js
+++ functions/db/Models/Inventory/Sale.js
@@ -35,18 +35,21 @@ sale.methods.toJSON = function () {
   delete obj.createdAt;
   delete obj.updatedAt;
 
-  
   // Convertir fecha a local
-  obj.date_format = new Date(obj.date).toLocaleString("es-ES", {
-    timeZone: "America/El_Salvador",
-    hour12: true,
-  });
+  if (obj.date) {
+    obj.date_format = new Date(obj.date).toLocaleString("es-ES", {
+      timeZone: "America/El_Salvador",
+      hour12: true,
+    });
+  }
 
   // Convertir el total a dolar
-  obj.total_format = obj.total.toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD",
-  });
+  if (obj.total) {
+    obj.total_format = obj.total.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  }
   return obj;
 };
 
