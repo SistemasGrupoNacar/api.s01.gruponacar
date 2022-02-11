eff606dd2744895d556390344b1c7adfb7113ee1
JavierMorales0 <javiermoralesmelara@gmail.com>
2022-02-10 18:13:50 -0600

    Creado el endpoint para cambiar la contrasena

--- functions/API/General/users.js
+++ functions/API/General/users.js
@@ -3,8 +3,9 @@ const route = express.Router();
 const { errors } = require("../../middleware/errors");
 const User = require("../../db/Models/General/User");
 const Role = require("../../db/Models/General/Role");
-const { body } = require("express-validator");
-const { createHash } = require("../../scripts/encrypt");
+const { body, validationResult } = require("express-validator");
+let { authenticateToken } = require("../../middleware/auth");
+const { comparePassword, createHash } = require("../../scripts/encrypt.js");
 
 route.get("/", async (req, res) => {
   const users = await User.find()
@@ -54,6 +55,54 @@ route.post(
       });
       await createdUser.save();
       res.status(201).json(createdUser);
+    } catch (error) {
+      res.status(500).json({
+        name: error.name,
+        message: error.message,
+      });
+    }
+  }
+);
+
+// Cambiar la contrasenia de un usuario
+route.put(
+  "/change-password",
+  authenticateToken,
+  body("password").notEmpty().withMessage("Contraseña requerida"),
+  body("newPassword").notEmpty().withMessage("Contraseña nueva es requerida"),
+  body("newPassword")
+    .isLength({ min: 6 })
+    .withMessage("Contraseña nueva debe tener al menos 6 caracteres"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    try {
+      const { newPassword, password } = req.body;
+      // Obtener el usuario
+      const { _id } = req.user;
+      const user = await User.findById(_id);
+      // Verificar que el usuario exista
+      if (!user) {
+        return res.status(404).json({
+          name: "Usuario",
+          message: "El usuario no existe",
+        });
+      }
+      // Verificar que la contraseña sea correcta
+      if (!comparePassword(password, user.password)) {
+        return res.status(401).json({
+          name: "Contraseña",
+          message: "La contraseña es incorrecta",
+        });
+      }
+      // Encriptar la nueva contraseña
+      const encryptPass = createHash(newPassword);
+      // Actualizar el usuario
+      user.password = encryptPass;
+      let userN = await user.save();
+      res.status(200).json(userN);
     } catch (error) {
       res.status(500).json({
         name: error.name,
