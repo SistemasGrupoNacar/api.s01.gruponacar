489d96b67ce8917c3f89bf483306cb8616287372
JavierMorales0 <javiermoralesmelara@gmail.com>
2022-03-14 10:47:11 -0600

    fb

--- functions/API/Control/salaries.js
+++ functions/API/Control/salaries.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const Salary = require("../../db/Models/Control/Salary");
 const Production = require("../../db/Models/Inventory/Production");
+const Employee = require("../../db/Models/Control/Employee");
 
 router.get("/", async (req, res) => {
   try {
@@ -53,8 +54,16 @@ router.get("/last", async (req, res) => {
 // Obtener los salarios de un empleado solo de el mes actual
 router.get("/total/:employee", async (req, res) => {
   try {
+    const { employee } = req.params;
+    const employeeName = await Employee.findById(employee);
+    if (!employeeName) {
+      return res.status(404).json({
+        message: "Empleado no encontrado",
+      });
+    }
+
     const salaries = await Salary.find({
-      employee: req.params.employee,
+      employee: employee,
       createdAt: {
         $gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
         $lte: new Date(new Date().getFullYear(), new Date().getMonth() + 1),
@@ -63,16 +72,23 @@ router.get("/total/:employee", async (req, res) => {
       _id: 1,
       description: 1,
     });
+    if (salaries.length === 0) {
+      return res.status(404).json({
+        message: "No hay salarios para este mes",
+      });
+    }
     let total = 0;
     salaries.forEach((salary) => {
       total += salary.total;
     });
+
     // Darle formato de dolar estadounidense
     total = total.toLocaleString("en-US", {
       style: "currency",
       currency: "USD",
     });
     return res.status(200).json({
+      employee: employeeName,
       total: total,
     });
   } catch (error) {
