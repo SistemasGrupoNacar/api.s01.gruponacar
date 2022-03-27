5ecd16927bd1db4e9debcdd3eaf050a8d2e78055
JavierMorales0 <javiermoralesmelara@gmail.com>
2022-03-27 12:57:58 -0600

    Agregada la funcion de multiples sistemas de login desde el login principal

--- functions/API/General/login.js
+++ functions/API/General/login.js
@@ -2,7 +2,7 @@ const express = require("express");
 const route = express.Router();
 const { errors } = require("../../middleware/errors");
 const User = require("../../db/Models/General/User");
-const { body } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 const { comparePassword } = require("../../scripts/encrypt");
 const { setToken } = require("../../middleware/auth");
 let { authenticateToken } = require("../../middleware/auth");
@@ -20,31 +20,35 @@ route.post(
   "/",
   body("username").notEmpty().withMessage("Usuario requerido"),
   body("password").notEmpty().withMessage("Contraseña requerida"),
+  body("type").notEmpty().withMessage("Tipo requerido"),
   async (req, res) => {
-    errors.validationErrorResponse(req, res);
-    const { username, password } = req.body;
-    const user = await User.findOne({ username: username });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    const { username, password, type } = req.body;
+    const user = await User.findOne({ username: username }).populate("role");
     if (user) {
-      if (user.employee != null) {
-        const role = await Role.findById(user.role);
-        if (role._id == EMPLOYEE_ID) {
-          return res.status(401).json({
-            name: "Usuario",
-            message: "No tiene permisos para acceder a este portal",
+      // Verifica si el rol tiene acceso a la solicitud)
+      if (user.role.title === type) {
+        if (comparePassword(password, user.password)) {
+          const token = setToken({
+            _id: user._id,
+            username: user.username,
+            avatar: user.avatar,
+          });
+          return res.status(200).json(token);
+        } else {
+          return res.status(403).json({
+            name: "Error de autenticación",
+            message: "Usuario o contraseña incorrectos",
           });
         }
-      }
-      if (comparePassword(password, user.password)) {
-        const token = setToken({
-          _id: user._id,
-          username: user.username,
-          avatar: user.avatar,
-        });
-        return res.status(200).json(token);
       } else {
         return res.status(403).json({
           name: "Error de autenticación",
-          message: "Usuario o contraseña incorrectos",
+          message: "Su usuario no tiene acceso a esta sección",
         });
       }
     } else {
