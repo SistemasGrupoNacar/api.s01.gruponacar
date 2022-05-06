e707c164e74d885f1a69c71d6702f1aadb258b07
JavierMorales0 <javiermoralesmelara@gmail.com>
2022-05-06 15:11:50 -0600

    Agregada la nueva base de datos

--- functions/db/config.js
+++ functions/db/config.js
@@ -1,3 +1,4 @@
-const URI = process.env.CONNECTION_STRING;
+const URI =
+  "mongodb+srv://javiermoralesmelara:sistemas.nacar@gruponacar.kfpvz.mongodb.net/S01_gruponacar?retryWrites=true&w=majority";
 
 module.exports = { URI };
